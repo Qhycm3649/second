7f12f0123b0648c8876a72777a0b458946e6ef76
cm <cm@itcast.cn>
2020-12-18 19:12:51 +0800

    第二次将项目提交到本地仓库

--- src/main/java/com/xyou/demo/Demo1.java
+++ src/main/java/com/xyou/demo/Demo1.java
@@ -3,6 +3,8 @@ package com.xyou.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("我是灰太狼");
-        System.out.println("我是小白狼")；
+        System.out.println("我是小白狼");
+        System.out.println("谁是羊");
+        System.out.println("谁是狼");
     }
 }
